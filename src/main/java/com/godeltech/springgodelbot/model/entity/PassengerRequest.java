5e1d49b6fb2e0fe13bd2930504807ed8d0a6b98a
a.miachyn <a.miachyn@godeltech.com>
2022-10-17 20:02:44 +0300

    Changed placement @Builder in PassengerRequest

--- src/main/java/com/godeltech/springgodelbot/model/entity/PassengerRequest.java
+++ src/main/java/com/godeltech/springgodelbot/model/entity/PassengerRequest.java
@@ -6,16 +6,17 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.time.LocalDate;
 import java.util.List;
+
 @Entity
 @DiscriminatorValue("PASSENGER_REQUEST")
 public class PassengerRequest extends Request {
+
+    @Builder
     public PassengerRequest(Long id, Long offerId, List<String> cities, LocalDate firstDate,
                             LocalDate secondDate, Boolean needForDescription, Token token,
                             List<Activity> suitableActivities, Activity activity, String description) {
         super(id, offerId, cities, firstDate, secondDate, needForDescription, token, suitableActivities, activity, description);
     }
-
-    @Builder
 
 
     public PassengerRequest() {
