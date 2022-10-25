2257df121bb3cda3ea3d63c6c1b8a2ca6e7e0fd1
a.miachyn <a.miachyn@godeltech.com>
2022-10-25 21:06:26 +0300

    Removed ping task

--- src/main/java/com/godeltech/springgodelbot/scheduler/impl/PingTaskServiceImpl.java
+++ src/main/java/com/godeltech/springgodelbot/scheduler/impl/PingTaskServiceImpl.java
@@ -16,17 +16,17 @@ public class PingTaskServiceImpl implements PingTaskService {
     @Value("${pingtask.url}")
     private String urlForPing;
 
-    @Scheduled(fixedRateString = "${pingtask.period}")
-    @Override
-    public void pingMe() {
-        try {
-            URL url = new URL(urlForPing);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
-            connection.disconnect();
-        } catch (IOException e) {
-            log.error("Ping job exited with error");
-        }
-    }
+//    @Scheduled(fixedRateString = "${pingtask.period}")
+//    @Override
+//    public void pingMe() {
+//        try {
+//            URL url = new URL(urlForPing);
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.connect();
+//            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
+//            connection.disconnect();
+//        } catch (IOException e) {
+//            log.error("Ping job exited with error");
+//        }
+//    }
 }
